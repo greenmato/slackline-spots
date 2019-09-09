ddfe1ebb82a78d8b13b8eaeb9b3f4b8df8dfc7b0
greenmato <theegreenman@gmail.com>
2019-09-09 22:09:48 +0100

    Remove some old code

--- spots-api/map/urls.py
+++ spots-api/map/urls.py
@@ -8,10 +8,6 @@ from map.api import SpotsApi, SpotApi, RatingsApi, VotesApi
 app_name = 'map'
 urlpatterns = [
     path('', MapView.as_view(), name='index'),
-    # path('views/spot/<int:spot_id>/',           views.get,              name='get'),
-    # path('views/spots/create/',                 views.create,           name='create'),
-    # path('success/',                            views.success,          name='success'),
-    # path('failure/',                            views.failure,          name='failure'),
 
     path('spots/',                              SpotsApi.as_view()),
     path('spots/<int:spot_id>/',                SpotApi.as_view()),
